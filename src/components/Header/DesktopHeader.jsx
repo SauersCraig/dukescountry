99604a7e5b362996ab5f89ccf09abdf1674a4b06
Craig Stueber <cstueber@sauerbrands.com>
2024-08-08 15:56:49 -0400

    list for nav

--- src/components/Header/DesktopHeader.jsx
+++ src/components/Header/DesktopHeader.jsx
@@ -15,34 +15,38 @@ export function DesktopHeader() {
             WATCH THE VIDEO
           </a>
         </li>
-
-        <a
-          href="https://dukesmayo.com/blogs/recipes"
-          target="_blank"
-          aria-label="Learn more about Recipes on Duke's Mayo Main Site"
-        >
-          RECIPES
-        </a>
+        <li>
+          <a
+            href="https://dukesmayo.com/blogs/recipes"
+            target="_blank"
+            aria-label="Learn more about Recipes on Duke's Mayo Main Site"
+          >
+            RECIPES
+          </a>
+        </li>
       </ul>
       <figure className="item2">
         <img src={Logo} alt="Welcome to Duke's Country" className="logoDesk" />
       </figure>
       <ul className="item3 menuLinks">
-        <a
-          className="merchLink"
-          href="https://dukesmayo.com/collections/merchandise"
-          target="_blank"
-          aria-label="Buy Some Cool Duke's Mayo Merch on Duke's Mayo E-Com Site"
-        >
-          MERCH
-        </a>
-        <a
-          href="https://dukesmayo.com/pages/classic"
-          target="_blank"
-          aria-label="Learn more about Duke's Mayo Football games on Duke's Mayo E-Com Site"
-        >
-          DUKE'S MAYO BOWL
-        </a>
+        <li className="merchLink">
+          <a
+            href="https://dukesmayo.com/collections/merchandise"
+            target="_blank"
+            aria-label="Buy Some Cool Duke's Mayo Merch on Duke's Mayo E-Com Site"
+          >
+            MERCH
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://dukesmayo.com/pages/classic"
+            target="_blank"
+            aria-label="Learn more about Duke's Mayo Football games on Duke's Mayo E-Com Site"
+          >
+            DUKE'S MAYO BOWL
+          </a>
+        </li>
       </ul>
     </nav>
   );
