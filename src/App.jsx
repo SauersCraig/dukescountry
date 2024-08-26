0b8a30198bd360b50874f8645081369b89e15fc5
Craig Stueber <craigstueber@gmail.com>
2024-08-26 13:25:06 -0400

    Pixel working

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,15 @@ import ReactGA from "react-ga4";
 // Pages
 import { HomePage } from "./pages/HomePage/HomePage";
 import { AdminPage } from "./pages/AdminPage/AdminPage";
+
+// Componets
+import Facebook from "./utils/Facebook";
 const TRACKING_ID = "G-C9JHZKQ2G8";
 function App() {
   ReactGA.initialize(TRACKING_ID);
   return (
     <>
+      <Facebook />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/AdminPage" element={<AdminPage />} />
