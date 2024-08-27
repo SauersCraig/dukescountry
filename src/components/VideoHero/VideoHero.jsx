e6ba530ec744929eab5cb1970b14797305e2dd9e
Craig Stueber <craigstueber@gmail.com>
2024-08-27 14:09:17 -0400

    vtt

--- src/components/VideoHero/VideoHero.jsx
+++ src/components/VideoHero/VideoHero.jsx
@@ -2,34 +2,42 @@
 import "./VideoHero.styles.css";
 import Guys from "../../assets/gifs/videoGif-reduced.gif";
 import Video1 from "../../assets/video/30Second.mp4";
-import CC from "../../assets/video/CC30Secondfile.vtt";
+import CC from "../../assets/video/CC30SecondDS.vtt";
 
 export function VideoHero() {
   return (
     <header className="videoHeroContainer">
       <section role="banner">
-        <a
-          href="https://www.youtube.com/watch?v=A4D4d-cr5po"
-          target="_blank"
-          aria-label="Link takes you to a video about Duke's Country."
+        <video
+          controls
+          controlsList="nodownload noplaybackrate"
+          disablePictureInPicture
+          className="videoDiv heroBanner"
+          muted
+          autoPlay
+          loop
         >
-          <img
-            src={Guys}
-            className="heroBanner"
-            alt="two guys in a convertible with thier hands up, no shirts, with giant Duke's Mayo tatoo's on thier back"
+          <source type="video/mp4" src={Video1} />
+          <track
+            src={CC}
+            kind="subtitles"
+            label="English Subtitles"
+            srclang="en"
+            default
           />
-        </a>
+        </video>
       </section>
-      <video controls className="videoDiv">
-        <source type="video/mp4" src={Video1} />
-        <track
-          src={CC}
-          kind="subtitles"
-          label="English Subtitles"
-          srclang="en"
-          default
+      {/* <a
+        href="https://www.youtube.com/watch?v=A4D4d-cr5po"
+        target="_blank"
+        aria-label="Link takes you to a video about Duke's Country."
+      >
+        <img
+          src={Guys}
+          className="heroBanner"
+          alt="two guys in a convertible with thier hands up, no shirts, with giant Duke's Mayo tatoo's on thier back"
         />
-      </video>
+      </a> */}
       <h1> WELCOME TO DUKE'S COUNTRY. </h1>
     </header>
   );
