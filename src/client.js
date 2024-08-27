2f4cfecdd5f673f5c700190a4132b0021582608c
Craig Stueber <craigstueber@gmail.com>
2024-08-27 15:34:23 -0400

    env variables created

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://wlaozdmgwaaymllhazpm.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsYW96ZG1nd2FheW1sbGhhenBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0MzQxNjUsImV4cCI6MjAzODAxMDE2NX0.ojRPW02vsmOCQtT7OSX7qdNaO2FL_Ls7KXsodrUCjoI";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
