aec5d161ee7c520a0f8a7a85fdb1976b518584eb
Craig Stueber <cstueber@sauerbrands.com>
2024-08-28 13:42:07 -0400

    video uploaded

--- src/components/VideoHero/VideoHero.jsx
+++ src/components/VideoHero/VideoHero.jsx
@@ -1,22 +1,29 @@
 // Asset Imports
 import "./VideoHero.styles.css";
 import Guys from "../../assets/gifs/videoGif-reduced.gif";
+import CC from "../../assets/video/CC30SecondFileDS.vtt";
 
 export function VideoHero() {
   return (
     <header className="videoHeroContainer">
-      <section role="banner">
-        <a
-          href="https://www.youtube.com/watch?v=A4D4d-cr5po"
-          target="_blank"
-          aria-label="Link takes you to a video about Duke's Country."
+      <section role="banner heroBanner">
+        <video
+          controls
+          autoPlay
+          loop
+          muted
+          className="video30 heroBanner"
+          controlsList="nodownload noplaybackrate "
+          disablePictureInPicture
+          alt="A humorous 30-second ad featuring two guys on a whimsical journey, sharing mayonnaise and engaging in silly antics."
         >
-          <img
-            src={Guys}
-            className="heroBanner"
-            alt="two guys in a convertible with thier hands up, no shirts, with giant Duke's Mayo tatoo's on thier back"
-          />
-        </a>
+          <source
+            src="https://wlaozdmgwaaymllhazpm.supabase.co/storage/v1/object/public/video/2024_FC_Dukes_WTDC_23976_1920x1080_16x9_30_HD_1.mp4?t=2024-08-28T17%3A03%3A14.162Z"
+            type="video/mp4"
+          ></source>
+          <track default kind="captions" srclang="en" src={CC} />
+          <p>Your browser does not support the video tag.</p>
+        </video>
       </section>
 
       <h1> WELCOME TO DUKE'S COUNTRY. </h1>
