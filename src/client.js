b2cf6046d14d1967df08b735c8f332c2b7aef7c5
Craig Stueber <cstueber@sauerbrands.com>
2024-08-28 10:19:02 -0400

    push live

--- src/client.js
+++ src/client.js
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wlaozdmgwaaymllhazpm.supabase.co"
+const supabaseUrl = "https://wlaozdmgwaaymllhazpm.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-console.log(import.meta.env.VITE_SUPABASE_ANON_KEY)
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
